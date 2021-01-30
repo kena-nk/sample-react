f6feb90b00bf07c4281ed9b13162896d1bf2d910
nakamotokeina <keina_nakamoto@rebuild-inc.co.jp>
2021-01-30 15:17:13 +0900

    入力フォーム作った

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,26 @@
+import { useForm, NestedValue } from 'react-hook-form';
+import { List } from '../components/Todo/List';
+import { Input } from '../components/Todo/Input';
+
+type FormData = {
+  todo: string;
+};
+
 export default function Home() {
+  const { register, handleSubmit, reset } = useForm<FormData>({
+    defaultValues: {
+      todo: '',
+    },
+  });
+  const onSubmit = handleSubmit(({todo}) => {
+    console.log('submit:', todo);
+    reset();
+  });
   return (
     <div>
-        <p>Hello, World.</p>
+      <form onSubmit={onSubmit}>
+        <Input name="todo" ref={register} />
+      </form>
     </div>
   )
 }
